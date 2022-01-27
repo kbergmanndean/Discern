6c35ad8b3e46efea1598e99862bea64a54aef99c
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-27 18:08:50 -0500

    tried two methods for converting csv to json data

--- src/App.js
+++ src/App.js
@@ -12,13 +12,41 @@ function App() {
     let headers = array[0].split(", ")
     for (let i = 1; i < array.length-1;i++) {
       let obj = {}
-      let str = array[i].split(",")
-      for (let j = 0; j < headers.length; j++){
-        obj[headers[j]] = str[j]
-      }
-      result.push(obj)
+      let str = array[i]
+//       let s = ''
+//       let flag = 0
+//     for (let ch of str) {
+//       if (ch === '"' && flag === 0) {
+//         flag = 1
+//       }
+//       else if (ch === '"' && flag == 1) flag = 0
+//       if (ch === ', ' && flag === 0) ch = '|'
+//       if (ch !== '"') s += ch
+//     }
+//     let properties = s.split("|")
+//     for (let j in headers) {
+//       if (properties[j].includes(", ")) {
+//        obj[headers[j]] = properties[j]
+//           .split(", ").map(item => item.trim())
+//       }
+//       else obj[headers[j]] = properties[j]
+//       result.push(obj)
+//       let json = JSON.stringify(result)
+//       console.log(json)
+//     }
+//   }
+// }
+    
+  
+      // let str = array[i].split(",")
+      // for (let j = 0; j < headers.length; j++){
+      //   obj[headers[j]] = str[j]
+      // }
+      // result.push(obj)
+      // let json = JSON.stringify(result)
+      // console.log(json)
     }
-    console.log(result)
+    
   }
 
   
