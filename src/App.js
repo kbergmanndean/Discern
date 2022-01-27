24660a218d12df7aca1550dd77efd8f3a4fbd16a
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-27 17:55:52 -0500

    attempt at converting csv data to json

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import axios from "axios";
 import {useState} from "react"
 
 function App() {
-  const [AAPLdata, setAAPLData] = useState([])
+  const [AAPLData, setAAPLData] = useState([])
+
+  function processData(data) {
+    let array = data.split("\n");
+    let result = []
+    let headers = array[0].split(", ")
+    for (let i = 1; i < array.length-1;i++) {
+      let obj = {}
+      let str = array[i].split(",")
+      for (let j = 0; j < headers.length; j++){
+        obj[headers[j]] = str[j]
+      }
+      result.push(obj)
+    }
+    console.log(result)
+  }
+
+  
 
   axios.get("/data/AAPL.csv")
     .then((response) => {
-      setAAPLData(response.data)
-      console.log(AAPLData)
+      // setAAPLData(response.data.toString().split("\r"))
+      processData(response.data)
+      
+      
     })
     .catch((error) => {
       console.log(error)
-    })
+  })
 
 
 
