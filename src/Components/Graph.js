3a61491fd0867d5318d973db4c032826e63dd2bf
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-29 16:11:19 -0500

    started styling

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -14,38 +14,44 @@ function Graph({AAPLData, AMZNData, FBData, GOOGData, GSPCData, NFLXData}) {
 
     let AAPLLine={
         label:"AAPL", 
-        backgroundColor:"red", 
-        borderColor:"red", 
+        backgroundColor:"#ff595e", 
+        borderColor:"#ff595e",
+        pointRadius:"2",
         data:closeData[0]
     }
     let AMZNLine={
         label:"AMZN",
-        backgroundColor:"yellow",
-        borderColor:"yellow",
+        backgroundColor:"#ffca3a",
+        borderColor:"#ffca3a",
+        pointRadius:"2",
         data:closeData[1]
     }
     let FBLine={
         label:"FB",
-        backgroundColor:"green",
-        borderColor:"green",
+        backgroundColor:"#8ac926",
+        borderColor:"#8ac926",
+        pointRadius:"2",
         data:closeData[2]
     }
     let GOOGLine={
         label:"GOOG",
-        backgroundColor:"blue",
-        borderColor:"blue",
+        backgroundColor:"#1982c4",
+        borderColor:"#1982c4",
+        pointRadius:"2",
         data:closeData[3]
     }
     let GSPCLine={
         label:"^GSPC",
-        backgroundColor:"purple",
-        borderColor:"purple",
+        backgroundColor:"black",
+        borderColor:"black",
+        pointRadius:"2",
         data:closeData[4]
     }
     let NFLXLine={
         label:"NFLX",
-        backgroundColor:"orange",
-        borderColor:"orange",
+        backgroundColor:"#6a4c93",
+        borderColor:"#6a4c93",
+        pointRadius:"2",
         data:closeData[5]
     }
     
@@ -56,11 +62,26 @@ function Graph({AAPLData, AMZNData, FBData, GOOGData, GSPCData, NFLXData}) {
         datasets:lineFiltered.length!=0 ? [lineFiltered, GSPCLine] : linesArray
     }
 
+    let options={
+        responsive:true, 
+        plugins:{
+            legend:{
+                position:"top",
+            },
+            title:{
+                display:true
+                // text:"Stock Prices & S&P 500 Index Value"
+            }
+        }
+    }
+
     const filterLines = (line) => {lineFiltered.label != line.label ? setLineFiltered(line) : setLineFiltered([])}
 
     return(
         <div>
-            <button onClick={()=>setShowComparison(!showComparison)}>Comparison</button>
+            <div id="head">
+            <h1>Stock Prices and S&P 500 Index Value</h1>
+            <button id="compare" onClick={()=>setShowComparison(!showComparison)}>Comparison</button>
             {showComparison?
             <div>
                 <button onClick={()=>filterLines(AAPLLine)}>AAPL</button>
@@ -70,7 +91,8 @@ function Graph({AAPLData, AMZNData, FBData, GOOGData, GSPCData, NFLXData}) {
                 <button onClick={()=>filterLines(NFLXLine)}>NFLX</button>
             </div>
             :null}
-            <Line data={data}/>
+            </div>
+            <Line data={data} options={options}/>
         </div>
     )
 }
